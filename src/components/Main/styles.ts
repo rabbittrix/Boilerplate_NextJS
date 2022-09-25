096aef217789a3a264766dd5b1a6860dce6f5c47
Alison Silva <alison.silva@wundermanthompson.com>
2022-09-25 20:59:03 +0100

    Revert "finalised test"
    This reverts commit 5463cb79ff5e9bb60d29cda2bd72203cc634e86a.

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
+export const Wrapper = styled.main`
+  background: -webkit-linear-gradient(#06092b, #cccccc);
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 export const Logo = styled.img`
   width: 25rem;
   margin-bottom: 2rem;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
-import Footer from 'components/Main/Footer'
-import Header from 'components/Main/Header'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import GlobalStyles from 'components/global'
+import GlobalStyles from 'styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -20,23 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <GlobalStyles />
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <Component {...pageProps} />
     </>
   )
 }
