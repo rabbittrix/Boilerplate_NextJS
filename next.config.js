7187460fe4c7859d60aa980d8b6e26924f3a1e7f
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2023-01-11 14:22:22 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
 
 module.exports = withPWA({
-  swcMinify: true,
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
+  // next.js config
 })
